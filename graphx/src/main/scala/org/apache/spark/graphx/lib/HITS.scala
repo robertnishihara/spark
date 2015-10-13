d47e04a6ee2735c6bf2e4c9d20d5982b76d3d0a1
Robert Nishihara <robertnishihara@gmail.com>
2015-10-13 11:03:55 -0700

    added fixes, optimizations, tests, and documentation

--- graphx/src/main/scala/org/apache/spark/graphx/lib/HITS.scala
+++ graphx/src/main/scala/org/apache/spark/graphx/lib/HITS.scala
@@ -5,31 +5,46 @@ import scala.reflect.ClassTag
 import org.apache.spark.Logging
 import org.apache.spark.graphx._
 
+/*
+ * Hyperlink-Induced Topic Search algorithm implementation.
+ */
 object HITS extends Logging {
 
+  /**
+   * Run HITS for a fixed number of iterations returning a graph
+   * with vertex attributes containing the authority and hub scores.
+   *
+   * @tparam VD the original vertex attribute (not used)
+   * @tparam ED the original edge attribute (not used)
+   *
+   * @param graph the graph on which to compute HITS
+   * @param numIter the number of iterations of HITS to run
+   *
+   * @return the graph containing with each vertex containing a tuple of
+   * the authority score and the hub score.
+   */
   def run[VD: ClassTag, ED: ClassTag](
       graph: Graph[VD, ED], numIter: Int): Graph[(Double, Double), Unit] =
   {
-
     // Initialize authority and hub scores, the first value is the authority
     // score and the second is the hub score. Initialize the edge message to unit
     var rankGraph: Graph[(Double, Double), Unit] = graph
       .mapVertices((vid, vdata) => (1D, 1D))
-      .mapTriplets(e => ())
+      .mapEdges(e => ())
 
     var iteration = 0
     while (iteration < numIter) {
       rankGraph.cache()
       // update authority scores
       val authority_updates = rankGraph
-        .aggregateMessages[Double](ctx => ctx.sendToDst(ctx.srcAttr._2), _ + _)
+        .aggregateMessages[Double](ctx => ctx.sendToDst(ctx.srcAttr._2), _ + _, TripletFields.Src)
       rankGraph = rankGraph.outerJoinVertices(authority_updates) {
         (vid, scores, authority_update) => (authority_update.getOrElse(0), scores._2)
       }
 
       // update hub scores
       val hub_updates = rankGraph.aggregateMessages[Double](
-        ctx => ctx.sendToDst(ctx.srcAttr._1), _ + _
+        ctx => ctx.sendToSrc(ctx.dstAttr._1), _ + _, TripletFields.Dst
       )
       rankGraph = rankGraph.outerJoinVertices(hub_updates) {
         (vid, scores, hub_update) => (scores._1, hub_update.getOrElse(0))
@@ -38,10 +53,10 @@ object HITS extends Logging {
       // normalize scores
       val authority_total = rankGraph.vertices
         .map { case (vid, vdata) => vdata._1 }
-        .reduce((a, b) => a + b)
+        .sum()
       val hub_total = rankGraph.vertices
         .map { case (vid, vdata) => vdata._2 }
-        .reduce((a, b) => a + b)
+        .sum()
       if (authority_total != 0) {
         rankGraph = rankGraph.mapVertices((vid, vdata) => (vdata._1 / authority_total, vdata._2))
       }
--- graphx/src/test/scala/org/apache/spark/graphx/lib/HITSSuite.scala
+++ graphx/src/test/scala/org/apache/spark/graphx/lib/HITSSuite.scala
@@ -2,14 +2,13 @@ package org.apache.spark.graphx.lib
 
 import org.apache.spark.SparkFunSuite
 import org.apache.spark.graphx._
-import org.apache.spark.graphx.util.GraphGenerators
+//import org.apache.spark.graphx.util.GraphGenerators
 
 // unnecessary?
-import org.apache.spark._
-import org.apache.spark.graphx._
-import org.apache.spark.graphx.lib.HITS
-import org.apache.spark.rdd.RDD
-
+//import org.apache.spark._
+//import org.apache.spark.graphx._
+//import org.apache.spark.graphx.lib.HITS
+//import org.apache.spark.rdd.RDD
 
 class HITSSuite extends SparkFunSuite with LocalSparkContext {
 
@@ -32,7 +31,6 @@ class HITSSuite extends SparkFunSuite with LocalSparkContext {
       )
       val graph = Graph(vertices, edges)
 
-
       val rankGraph0 = HITS.run(graph, 0)
       val verticesGroundTruth0: RDD[(VertexId, (Double, Double))] = sc.parallelize(
         Array((1L, (1.0, 1.0)), (2L, (1.0, 1.0)), (3L, (1.0, 1.0)),
@@ -42,15 +40,48 @@ class HITSSuite extends SparkFunSuite with LocalSparkContext {
 
       val rankGraph1 = HITS.run(graph, 1)
       val verticesGroundTruth1: RDD[(VertexId, (Double, Double))] = sc.parallelize(
-        Array((1L, (0.0, 0.0)), (2L, (1.0 / 4.0, 0.0)), (3L, (1.0 / 4.0, 1.0 / 3.0)),
-          (4L, (1.0 / 4.0, 1.0 / 3.0)), (5L, (1.0 / 4.0, 1.0 / 3.0)))
+        Array((1L, (0.0, 1.0 / 4.0)), (2L, (1.0 / 4.0, 1.0 / 4.0)), (3L, (1.0 / 4.0, 1.0 / 4.0)),
+          (4L, (1.0 / 4.0, 1.0 / 4.0)), (5L, (1.0 / 4.0, 0.0)))
       )
       assert(compareScores(rankGraph1.vertices, VertexRDD(verticesGroundTruth1)) < 1e-6)
 
       val rankGraph10 = HITS.run(graph, 10)
       val verticesGroundTruth10: RDD[(VertexId, (Double, Double))] = sc.parallelize(
-        Array((1L, (0.0, 0.0)), (2L, (0.0, 0.0)), (3L, (0.0, 0.0)),
-          (4L, (0.0, 0.0)), (5L, (0.0, 0.0)))
+        Array((1L, (0.0, 1.0 / 4.0)), (2L, (1.0 / 4.0, 1.0 / 4.0)), (3L, (1.0 / 4.0, 1.0 / 4.0)),
+          (4L, (1.0 / 4.0, 1.0 / 4.0)), (5L, (1.0 / 4.0, 0.0)))
+      )
+      assert(compareScores(rankGraph10.vertices, VertexRDD(verticesGroundTruth10)) < 1e-6)
+    }
+  }
+
+  test("Ring HITS") {
+    withSpark { sc =>
+      val vertices: RDD[(VertexId, Unit)] = sc.parallelize(
+        Array((1L, ()), (2L, ()), (3L, ()), (4L, ()))
+      )
+      val edges: RDD[Edge[Unit]] = sc.parallelize(
+        Array(Edge(1L, 2L, ()), Edge(2L, 3L, ()), Edge(3L, 4L, ()), Edge(4L, 1L, ()))
+      )
+      val graph = Graph(vertices, edges)
+
+      val rankGraph0 = HITS.run(graph, 0)
+      val verticesGroundTruth0: RDD[(VertexId, (Double, Double))] = sc.parallelize(
+        Array((1L, (1.0, 1.0)), (2L, (1.0, 1.0)), (3L, (1.0, 1.0)),
+          (4L, (1.0, 1.0)))
+      )
+      assert(compareScores(rankGraph0.vertices, VertexRDD(verticesGroundTruth0)) < 1e-6)
+
+      val rankGraph1 = HITS.run(graph, 1)
+      val verticesGroundTruth1: RDD[(VertexId, (Double, Double))] = sc.parallelize(
+        Array((1L, (1.0 / 4.0, 1.0 / 4.0)), (2L, (1.0 / 4.0, 1.0 / 4.0)), (3L, (1.0 / 4.0, 1.0 / 4.0)),
+          (4L, (1.0 / 4.0, 1.0 / 4.0)))
+      )
+      assert(compareScores(rankGraph1.vertices, VertexRDD(verticesGroundTruth1)) < 1e-6)
+
+      val rankGraph10 = HITS.run(graph, 10)
+      val verticesGroundTruth10: RDD[(VertexId, (Double, Double))] = sc.parallelize(
+        Array((1L, (1.0 / 4.0, 1.0 / 4.0)), (2L, (1.0 / 4.0, 1.0 / 4.0)), (3L, (1.0 / 4.0, 1.0 / 4.0)),
+          (4L, (1.0 / 4.0, 1.0 / 4.0)))
       )
       assert(compareScores(rankGraph10.vertices, VertexRDD(verticesGroundTruth10)) < 1e-6)
     }
